9689eeb3312b6d4b16e2a932bf22c05f2c492a65
nikanoza <nikanozadze1010@gmail.com>
2022-08-09 00:20:07 +0400

    feat: add socia-icons on home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Logo } from 'assets';
+import { Fb, Logo, Twitter, YouTube } from 'assets';
 
 const Home = () => {
   return (
@@ -17,38 +17,37 @@ const Home = () => {
       </header>
       <section
         id='animation-and-info'
-        className='h-[calc(100%-100px)] w-full pt-11 flex justify-between items-center'
+        className='h-5/6 w-full pt-11 flex justify-between items-center'
       >
         <div id='solar-system'></div>
-        <div id='info' className='w-5/12 bg-[#FBD560] h-2/3 rounded-[20px]'>
-          <div id='info-top' className='flex justify-between'>
-            <div
-              id='circle-sm'
-              className='w-4 h-4 bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] rounded-full mt-4 ml-5'
-            ></div>
-            <div
-              id='image-box'
-              className='w-80 h-80 rounded-full border border-[solid #FFFFFF] -mt-40 bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] drop-shadow-[2px_4px_14px_#000000]'
-            ></div>
-            <div
-              id='circle-sm'
-              className='w-4 h-4 bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] rounded-full mt-4 mr-5'
-            ></div>
+        <div className='w-5/12 h-5/6 mt-48'>
+          <div id='info' className='w-full bg-[#FBD560] h-full rounded-[20px]'>
+            <div id='info-top' className='flex justify-between'>
+              <div
+                id='circle-sm'
+                className='w-4 h-4 bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] rounded-full mt-4 ml-5'
+              ></div>
+              <div
+                id='image-box'
+                className='w-80 h-80 rounded-full border border-[solid #FFFFFF] -mt-40 bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] drop-shadow-[2px_4px_14px_#000000] flex justify-center items-center'
+              >
+                <img src={Logo} alt='' className='w-64 h-32' />
+              </div>
+              <div
+                id='circle-sm'
+                className='w-4 h-4 bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] rounded-full mt-4 mr-5'
+              ></div>
+            </div>
+            <p
+              id='info-text'
+              className='font-arial pl-16 mr-10 pr-10 mt-9 text-lg text-justify overflow-y-auto max-h-96'
+            ></p>
           </div>
-          <p id='info-text' className='font-arial pl-16 pr-20 mt-9 text-lg'>
-            დაწყვილების პერიოდი ზომიერ და არქტიკულ რეგიონებში მობინადრე
-            დათვებისთვის, ჩვეულებრივ, გაზაფხულია. მაკეობა ხანმოკლეა, თუმცა იმის
-            გამო, რომ დათვი არ მშობიარობს მანამ, სანამ ზამთრის შუა ძილში არ
-            იქნება, განაყოფიერებული კვერცხუჯრედის საშვილოსნოში იმპლანტაცია ხდება
-            მხოლოდ ოქტომბე-ნოემბერში, ამ პროცესს „დაგვიანებული იმპლანტაცია“
-            ეწოდება.
-            <br /> <br />
-            დათვი შობს წარმოუდგენლად პატარა ბელებს, ხშირ შემთხვევაში — ორს.
-            ახალშობილები მხოლოდ 200-700 გრამს იწონიან და ძალიან ჰგვანან პატარა
-            ვირთხებს. ისინი თვალაუხელელნი, უკბილონი და ბეწვის გარეშე იბადებიან.
-            პატარები რჩებიან რა ბუნაგში დედასთან, მისი ნოყიერი რძით იკვებებიან
-            და სწრაფად.
-          </p>
+          <div className='h-9 mt-6 flex justify-center gap-x-12'>
+            <img src={Fb} alt='' />
+            <img src={YouTube} alt='' />
+            <img src={Twitter} alt='' />
+          </div>
         </div>
       </section>
     </div>
