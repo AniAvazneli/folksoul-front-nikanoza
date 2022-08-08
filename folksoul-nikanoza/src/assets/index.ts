10ca4a57774c3282be85adf3b150143d317ec5a2
nikanoza <81360200+nikanoza@users.noreply.github.com>
2022-08-08 10:15:12 +0400

    fix: delete extra folksoul-nikanoza folder

--- folksoul-nikanoza/src/assets/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Logo } from './logo.png';
-export { default as Fb } from './fb.png';
-export { default as Twitter } from './twitter.png';
-export { default as YouTube } from './Youtube.png';
