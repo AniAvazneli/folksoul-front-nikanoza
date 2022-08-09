8d955ef2d425af2c2a41b7c5ee0e26511dcb06bf
nikanoza <nikanozadze1010@gmail.com>
2022-08-09 10:08:23 +0400

    feat: add react router

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <div
+      id='login-page'
+      className=' w-full h-full bg-[radial-gradient(50%_50%_at_50%_50%,_#534571_0%,_#342C46_100%)] flex justify-center items-center'
+    >
+      <form id='login-form'></form>
+    </div>
+  );
+};
+
+export default Login;
