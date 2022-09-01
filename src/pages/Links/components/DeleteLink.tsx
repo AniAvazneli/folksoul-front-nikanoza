52259a03f14c4aab2d1341dce86e0722caffa86d
nikanoza <nikanozadze1010@gmail.com>
2022-09-01 10:48:33 +0400

    feat: add link delete modal

---
+++ src/pages/Links/components/DeleteLink.tsx
@@ -0,0 +1,40 @@
+import { Close } from 'assets';
+import { Button, InfoHeader } from 'components';
+import { LinkFormValues } from 'types/forms';
+
+const DeleteLink: React.FC<{
+  close: () => void;
+  link: LinkFormValues;
+}> = (props) => {
+  const closeModalHandler = () => {
+    props.close();
+  };
+
+  const linkDeleteHandler = () => {};
+
+  return (
+    <div className='flex flex-col items-center p-4'>
+      <Button
+        id='close-add-modal-btn'
+        onClick={closeModalHandler}
+        className='ml-auto'
+        type='button'
+      >
+        <img src={Close} alt='' />
+      </Button>
+      <InfoHeader>გსურთ ბმულის წაშლა?</InfoHeader>
+      <div className='flex gap-10 mt-24'>
+        <Button
+          className='w-40 h-10 bg-[#EB5757] rounded-md mt-20 font-ninoMtavruli text-white text-lg'
+          id='link-del-btn'
+          type='button'
+          onClick={linkDeleteHandler}
+        >
+          წაშლა
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteLink;
