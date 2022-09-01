79e051dd39c49fdb071f4aaca016c7998b0d3671
nikanoza <nikanozadze1010@gmail.com>
2022-09-01 10:27:41 +0400

    fix: change some image names, use kebab-case

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,6 @@
-export { default as Logo } from './logo.png';
-export { default as Fb } from './fb.png';
-export { default as Twitter } from './twitter.png';
+export { default as Logo } from './Logo.png';
+export { default as Fb } from './Fb.png';
+export { default as Twitter } from './Twitter.png';
 export { default as YouTube } from './Youtube.png';
 export { default as Heading } from './Heading.png';
 export { default as Door } from './Door.png';
